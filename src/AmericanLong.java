59177303691ee2858f2867e087f9f0da7a04ac3e
Collin Strassburg <maddog241996@gmail.com>
2017-05-08 22:12:15 -0400

    Created a failing test for thousands place

--- src/AmericanLong.java
+++ src/AmericanLong.java
@@ -1,26 +1,22 @@
 public class AmericanLong{
-	private static String[] ones = {"ty","one","two","three","four","five","six","seven","eight","nine"};
+	private static String[] ones = {"","one","two","three","four","five","six","seven","eight","nine"};
 	private static String[] teens = {"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-	private static String[] tens = {"ty", "ten","twen","thir","for","fif","six","seven","eigh","nine"};
+	private static String[] tens = {"", "ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
 	public static String one(int a){
 		String b = null;
 		if(a < 10){
 			if(a != 0){
-				//String number = ones[a];
 				b = ones[a];
-				System.out.println(b);
 				return b;
 			}
 			else{
 				b = "zero";
-				System.out.println(b);
 				return b;
 			}
 		}
 		else{
 			if(a < 20){
 				b = teens[a - 10];
-				System.out.println(b);
 				return b;
 			}
 			else{
@@ -28,13 +24,10 @@ public class AmericanLong{
 					String aSplittable = Integer.toString(a);
 					int cTensPlace = Character.getNumericValue(aSplittable.charAt(0));
 					int cOnesPlace = Character.getNumericValue(aSplittable.charAt(1));
+					b = tens[cTensPlace];
 					if(cOnesPlace > 0){
-						b = tens[cTensPlace] + tens[0] + " " + ones[cOnesPlace];
+						b = b + " " + ones[cOnesPlace];
 					}
-					else{
-						b = tens[cTensPlace] + tens[0];
-					}
-					System.out.println(b);
 					return b;
 				}
 				else{
@@ -45,14 +38,272 @@ public class AmericanLong{
 						int cOnesPlace = Character.getNumericValue(aSplittable.charAt(2));
 						b = ones[cHundredsPlace] + " hundred";
 						if(cTensPlace != 0){
-							b = b + " " + tens[cTensPlace] + ones[0];
+							b = b + " " + tens[cTensPlace];
 						}
 						if(cOnesPlace != 0){
 							b = b +  " " + ones[cOnesPlace];
 						}
-					
+						return b;
 					}
-					System.out.println(b);
+					else{
+						if(a < 10000){                                                                         
+							String aSplittable = Integer.toString(a);
+							int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(0));
+							int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(1));
+							int cTensPlace = Character.getNumericValue(aSplittable.charAt(2));
+							int cOnesPlace = Character.getNumericValue(aSplittable.charAt(3));
+							b = ones[cThousandsPlace] + " thousand";
+							if(cHundredsPlace != 0){
+								b = b + " " + ones[cHundredsPlace] + " hundred";
+							}
+							if(cTensPlace != 0){
+								b = b + " " + tens[cTensPlace];
+							}
+							if(cOnesPlace != 0){
+								b = b +  " " + ones[cOnesPlace];
+							}
+							//System.out.println(b);
+							return b;
+						}
+						/**
+						else{
+							if(a < 100000){                                                                         
+								String aSplittable = Integer.toString(a);
+								int cTenThousandsPlace = Character.getNumericValue(aSplittable.charAt(0));
+								int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(1));
+								if(cTenThousandsPlace > 1){
+									b = tens[cTenThousandsPlace] + " " + ones[cThousandsPlace];
+								}
+								else if(cTenThousandsPlace == 1){
+									b = teens[cTenThousandsPlace];
+								}
+								int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(2));
+								int cTensPlace = Character.getNumericValue(aSplittable.charAt(3));
+								int cOnesPlace = Character.getNumericValue(aSplittable.charAt(4));
+								b = b + " thousand";
+								if(cHundredsPlace != 0){
+									b = b + " " + ones[cHundredsPlace] + " hundred";
+								}
+								if(cTensPlace != 0){
+									b = b + " " + tens[cTensPlace];
+								}
+								if(cOnesPlace != 0){
+									b = b +  " " + ones[cOnesPlace];
+								}
+								//System.out.println(b);
+								return b;
+							}
+							else{
+								if(a < 1000000){                                                                         
+									String aSplittable = Integer.toString(a);
+									int cHundredThousandsPlace = Character.getNumericValue(aSplittable.charAt(0));
+									int cTenThousandsPlace = Character.getNumericValue(aSplittable.charAt(1));
+									int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(2));
+									int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(3));
+									int cTensPlace = Character.getNumericValue(aSplittable.charAt(4));
+									int cOnesPlace = Character.getNumericValue(aSplittable.charAt(5));
+									b = ones[cHundredThousandsPlace] + " hundred";
+									if(cTenThousandsPlace > 1 || cTenThousandsPlace == 0){
+										b = b + " " + tens[cTenThousandsPlace] + " " + ones[cThousandsPlace];
+									}
+									else if(cTenThousandsPlace == 1){
+										b = b + " " + teens[cTenThousandsPlace];
+									}
+									if(cHundredThousandsPlace > 0 || cTenThousandsPlace > 0 || cThousandsPlace > 0){
+										b = b + " thousand";
+									}
+									if(cHundredsPlace != 0){
+										b = b + " " + ones[cHundredsPlace] + " hundred";
+									}
+									if(cTensPlace != 0){
+										b = b + " " + tens[cTensPlace];
+									}
+									if(cOnesPlace != 0){
+										b = b +  " " + ones[cOnesPlace];
+									}
+									//System.out.println(b);
+									return b;
+								}
+								else{
+									if(a < 10000000){                                                                         
+										String aSplittable = Integer.toString(a);
+										int cMillionsPlace = Character.getNumericValue(aSplittable.charAt(0));
+										int cHundredThousandsPlace = Character.getNumericValue(aSplittable.charAt(1));
+										int cTenThousandsPlace = Character.getNumericValue(aSplittable.charAt(2));
+										int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(3));
+										int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(4));
+										int cTensPlace = Character.getNumericValue(aSplittable.charAt(5));
+										int cOnesPlace = Character.getNumericValue(aSplittable.charAt(6));
+										b = ones[cMillionsPlace] + " million";
+										if(cHundredThousandsPlace != 0){
+											b = b + " " + ones[cHundredThousandsPlace] + " hundred";
+										}
+										if(cTenThousandsPlace > 1 || cTenThousandsPlace == 0){
+											b = b + " " + tens[cTenThousandsPlace] + " " + ones[cThousandsPlace];
+										}
+										else if(cTenThousandsPlace == 1){
+											b = b + " " + teens[cTenThousandsPlace];
+										}
+										if(cHundredThousandsPlace > 0 || cTenThousandsPlace > 0 || cThousandsPlace > 0){
+											b = b + " thousand";
+										}
+										if(cHundredsPlace != 0){
+											b = b + " " + ones[cHundredsPlace] + " hundred";
+										}
+										if(cTensPlace != 0){
+											b = b + " " + tens[cTensPlace];
+										}
+										if(cOnesPlace != 0){
+											b = b +  " " + ones[cOnesPlace];
+										}
+										//System.out.println(b);
+										return b;
+									}
+									else{
+										if(a < 100000000){                                                                         
+											String aSplittable = Integer.toString(a);
+											int cTenMillionsPlace = Character.getNumericValue(aSplittable.charAt(0));
+											int cMillionsPlace = Character.getNumericValue(aSplittable.charAt(1));
+											int cHundredThousandsPlace = Character.getNumericValue(aSplittable.charAt(2));
+											int cTenThousandsPlace = Character.getNumericValue(aSplittable.charAt(3));
+											int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(4));
+											int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(5));
+											int cTensPlace = Character.getNumericValue(aSplittable.charAt(6));
+											int cOnesPlace = Character.getNumericValue(aSplittable.charAt(7));
+											if(cTenMillionsPlace > 1 || cMillionsPlace == 0){
+												b = tens[cTenMillionsPlace] + " " + ones[cMillionsPlace];
+											}
+											else if(cMillionsPlace == 1){
+												b = teens[cTenMillionsPlace];
+											}
+											b = b + " million";
+											if(cHundredThousandsPlace != 0){
+												b = b + " " + ones[cHundredThousandsPlace] + " hundred";
+											}
+											if(cTenThousandsPlace > 1 || cTenThousandsPlace == 0){
+												b = b + " " + tens[cTenThousandsPlace] + " " + ones[cThousandsPlace];
+											}
+											else if(cTenThousandsPlace == 1){
+												b = b + " " + teens[cTenThousandsPlace];
+											}
+											if(cHundredThousandsPlace > 0 || cTenThousandsPlace > 0 || cThousandsPlace > 0){
+												b = b + " thousand";
+											}
+											if(cHundredsPlace != 0){
+												b = b + " " + ones[cHundredsPlace] + " hundred";
+											}
+											if(cTensPlace != 0){
+												b = b + " " + tens[cTensPlace];
+											}
+											if(cOnesPlace != 0){
+												b = b +  " " + ones[cOnesPlace];
+											}
+											//System.out.println(b);
+											return b;
+										}
+										else{
+											if(a < 1000000000){                                                                         
+												String aSplittable = Integer.toString(a);
+												int cHundredMillionsPlace = Character.getNumericValue(aSplittable.charAt(0));
+												int cTenMillionsPlace = Character.getNumericValue(aSplittable.charAt(1));
+												int cMillionsPlace = Character.getNumericValue(aSplittable.charAt(2));
+												int cHundredThousandsPlace = Character.getNumericValue(aSplittable.charAt(3));
+												int cTenThousandsPlace = Character.getNumericValue(aSplittable.charAt(4));
+												int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(5));
+												int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(6));
+												int cTensPlace = Character.getNumericValue(aSplittable.charAt(7));
+												int cOnesPlace = Character.getNumericValue(aSplittable.charAt(8));
+												b = ones[cHundredMillionsPlace] + " hundred";
+												if(cTenMillionsPlace > 1 || cMillionsPlace == 0){
+													b = b + " " + tens[cTenMillionsPlace] + " " + ones[cMillionsPlace];
+												}
+												else if(cMillionsPlace == 1){
+													b = b + " " + teens[cTenMillionsPlace];
+												}
+												b = b + " million";
+												if(cHundredThousandsPlace != 0){
+													b = b + " " + ones[cHundredThousandsPlace] + " hundred";
+												}
+												if(cTenThousandsPlace > 1 || cTenThousandsPlace == 0){
+													b = b + " " + tens[cTenThousandsPlace] + " " + ones[cThousandsPlace];
+												}
+												else if(cTenThousandsPlace == 1){
+													b = b + " " + teens[cTenThousandsPlace];
+												}
+												if(cHundredThousandsPlace > 0 || cTenThousandsPlace > 0 || cThousandsPlace > 0){
+													b = b + " thousand";
+												}
+												if(cHundredsPlace != 0){
+													b = b + " " + ones[cHundredsPlace] + " hundred";
+												}
+												if(cTensPlace != 0){
+													b = b + " " + tens[cTensPlace];
+												}
+												if(cOnesPlace != 0){
+													b = b +  " " + ones[cOnesPlace];
+												}
+												//System.out.println(b);
+												return b;
+											}
+											else{
+												if(a <= 2147483647){                                                                         
+													String aSplittable = Integer.toString(a);
+													int cBillionsPlace = Character.getNumericValue(aSplittable.charAt(0));
+													int cHundredMillionsPlace = Character.getNumericValue(aSplittable.charAt(1));
+													int cTenMillionsPlace = Character.getNumericValue(aSplittable.charAt(2));
+													int cMillionsPlace = Character.getNumericValue(aSplittable.charAt(3));
+													int cHundredThousandsPlace = Character.getNumericValue(aSplittable.charAt(4));
+													int cTenThousandsPlace = Character.getNumericValue(aSplittable.charAt(5));
+													int cThousandsPlace = Character.getNumericValue(aSplittable.charAt(6));
+													int cHundredsPlace = Character.getNumericValue(aSplittable.charAt(7));
+													int cTensPlace = Character.getNumericValue(aSplittable.charAt(8));
+													int cOnesPlace = Character.getNumericValue(aSplittable.charAt(9));
+													b = ones[cBillionsPlace] + " billion";
+													if(cHundredMillionsPlace != 0){
+														b = b + " " + ones[cHundredMillionsPlace] + " hundred";
+													}
+													if(cTenMillionsPlace > 1 || cMillionsPlace == 0){
+														b = b + " " + tens[cTenMillionsPlace] + " " + ones[cMillionsPlace];
+													}
+													else if(cMillionsPlace == 1){
+														b = b + " " + teens[cTenMillionsPlace];
+													}
+													if(cHundredMillionsPlace > 0 || cTenMillionsPlace > 0 || cMillionsPlace > 0){
+														b = b + " million";
+													}
+													if(cHundredThousandsPlace != 0){
+														b = b + " " + ones[cHundredThousandsPlace] + " hundred";
+													}
+													if(cTenThousandsPlace > 1 || cTenThousandsPlace == 0){
+														b = b + " " + tens[cTenThousandsPlace] + " " + ones[cThousandsPlace];
+													}
+													else if(cTenThousandsPlace == 1){
+														b = b + " " + teens[cTenThousandsPlace];
+													}
+													if(cHundredThousandsPlace > 0 || cTenThousandsPlace > 0 || cThousandsPlace > 0){
+														b = b + " thousand";
+													}
+													if(cHundredsPlace != 0){
+														b = b + " " + ones[cHundredsPlace] + " hundred";
+													}
+													if(cTensPlace != 0){
+														b = b + " " + tens[cTensPlace];
+													}
+													if(cOnesPlace != 0){
+														b = b +  " " + ones[cOnesPlace];
+													}
+													//System.out.println(b);
+													System.out.println(b);
+													return b;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+						*/
+					}
 					return b;
 				}
 			}
--- src/AmericanLongTest.java
+++ src/AmericanLongTest.java
@@ -5,10 +5,19 @@ import org.junit.Test;
 public class AmericanLongTest{
 	private String zero;
 	private String one;
-	private String ten;
 	private String teen;
 	private String tens;
 	private String hundreds;
+	private String thousands;
+	private String tenthousands;
+	private String hundredthousands;
+	private String millions;
+	private String tenmillions;
+	private String hundredmillions;
+	private String billions;
+	private String tenbillions;
+	private String hundredbillions;
+	private String trillions;
 	@Test public void zero(){
 		try{
 			zero = AmericanLong.one(0);
@@ -54,4 +63,73 @@ public class AmericanLongTest{
 			e.getMessage();
 		}
 	}
+	@Test public void thousands(){
+		try{
+			thousands = AmericanLong.one(3691);
+			assertEquals("three thousand six hundred ninety", thousands);
+			//System.out.print(thousands);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	/**
+	@Test public void tenthousands(){
+		try{
+			tenthousands = AmericanLong.one(27176);
+			assertEquals("twenty seven thousand one hundred seventy six", tenthousands);
+			//System.out.print(tenthousands);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	@Test public void hundredthousands(){
+		try{
+			hundredthousands = AmericanLong.one(985621);
+			assertEquals("nine hundred eighty five thousand six hundred twenty one", hundredthousands);
+			//System.out.print(hundredthousands);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	@Test public void millions(){
+		try{
+			millions = AmericanLong.one(8234102);
+			assertEquals("eight million two hundred thirty four thousand one hundred two", millions);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	@Test public void tenmillions(){
+		try{
+			tenmillions = AmericanLong.one(36029145);
+			assertEquals("thirty six million twenty nine thousand one hundred forty five", tenmillions);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	@Test public void hundredmillions(){
+		try{
+			hundredmillions = AmericanLong.one(187465190);
+			assertEquals("one hundred eighty seven million four hundred sixty five thousand one hundred ninety", hundredmillions);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	@Test public void billions(){
+		try{
+			billions = AmericanLong.one(2147483647);
+			assertEquals("two billion one hundred forty seven million four hundred eighty three thousand six hundred forty seven", billions);
+			System.out.print(billions);
+		}
+		catch(Exception e){
+			e.getMessage();
+		}
+	}
+	*/
 }
